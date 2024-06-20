98c9351dc988140f3608f4b667335d254049c926
hasuburero <hasuburero0802@gmail.com>
2024-06-20 18:28:57 +0900

    2024.6.20.1 edit panic

--- sim/rm/except/panic.go
+++ sim/rm/except/panic.go
@@ -10,4 +10,11 @@ var Error error chan
 func PanicHandler(){
   ch := make(chan os.Signal, 1)
   Error = make(chan error, 1)
+
+  signal.Notify(ch, os.Interrupt)
+  go func(){
+    for sig := range ch{
+      fmt.Println()
+    }
+  }
 }
